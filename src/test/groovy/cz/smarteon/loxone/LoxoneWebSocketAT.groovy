933762bf99bfa1158bc7e40b90e2cfea09da8f67
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2018-04-10 17:02:44 +0200

    sanitize acceptance test run

--- src/test/groovy/cz/smarteon/loxone/LoxoneWebSocketAT.groovy
+++ src/test/groovy/cz/smarteon/loxone/LoxoneWebSocketAT.groovy
@@ -2,6 +2,7 @@ package cz.smarteon.loxone
 
 import cz.smarteon.loxone.message.LoxoneValue
 import org.bouncycastle.jce.provider.BouncyCastleProvider
+import spock.lang.Requires
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -9,25 +10,38 @@ import java.security.Security
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
+import static java.lang.System.getenv
 
+@Requires({(
+        env[LOX_ADDRESS]
+        && env[LOX_USER]
+        && env[LOX_PASS]
+        && env[LOX_VISPASS]
+        && env[LOX_ALARM]
+    )})
 class LoxoneWebSocketAT extends Specification {
 
-    private static final String ADDRESS = '192.168.88.246'
-    private static final String USER = 'showroom'
-    private static final String PASS = 'showroom'
-    private static final String VIS_PASS = '1234'
-    private static final String ALARM_ID = '0f86a2fe-0378-3e15-ffff373f9870b52a'
+    public static final String LOX_ADDRESS = 'LOX_ADDRESS'
+    public static final String LOX_USER = 'LOX_USER'
+    public static final String LOX_PASS = 'LOX_PASS'
+    public static final String LOX_VISPASS = 'LOX_VISPASS'
+    public static final String LOX_ALARM = 'LOX_ALARM'
 
     @Shared LoxoneWebSocket loxoneWebSocket
     @Shared CommandMemory commands
+    @Shared def alarmId
 
     void setupSpec() {
         Security.addProvider(new BouncyCastleProvider())
-        final LoxoneAuth loxoneAuth = new LoxoneAuth(new LoxoneHttp(ADDRESS), USER, PASS, VIS_PASS)
-        loxoneWebSocket = new LoxoneWebSocket(ADDRESS, loxoneAuth)
+
+        def address = getenv(LOX_ADDRESS)
+        final LoxoneAuth loxoneAuth = new LoxoneAuth(new LoxoneHttp(address), getenv(LOX_USER), getenv(LOX_PASS), getenv(LOX_VISPASS))
+        loxoneWebSocket = new LoxoneWebSocket(address, loxoneAuth)
 
         commands = new CommandMemory()
         loxoneWebSocket.registerListener(commands)
+
+        alarmId = getenv(LOX_ALARM)
     }
 
     void cleanup() {
@@ -36,10 +50,10 @@ class LoxoneWebSocketAT extends Specification {
 
     def "should ask for alarm status"() {
         given:
-        def latch = commands.expectCommand(".*/$ALARM_ID/all")
+        def latch = commands.expectCommand(".*/$alarmId/all")
 
         when:
-        loxoneWebSocket.sendSecureCommand("$ALARM_ID/all")
+        loxoneWebSocket.sendSecureCommand("$alarmId/all")
 
         then:
         latch.await(1, TimeUnit.SECONDS)

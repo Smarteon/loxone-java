1e313315dce00ca86729268c58f578db32bfde04
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2018-04-12 12:25:51 +0200

    introduce releasing and publishing to oss

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 group = "cz.smarteon"
-version = "0.0.1-SNAPSHOT"
 
 plugins {
     java
     groovy
-    `maven-publish`
+    signing
+    maven
+    id("net.researchgate.release") version "2.6.0"
 }
 
 java {
@@ -12,10 +13,10 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_7
 }
 
+
 repositories {
     mavenCentral()
 }
-
 
 dependencies {
     implementation("org.java-websocket:Java-WebSocket:1.3.8")
@@ -35,10 +36,91 @@ dependencies {
     testImplementation("org.bouncycastle:bcprov-jdk15on:1.59")
 }
 
-publishing {
-    (publications) {
-        "mavenJava"(MavenPublication::class) {
-            from(components["java"])
+release {
+    versionPropertyFile = "version.properties"
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    classifier = "sources"
+    from(java.sourceSets["main"].allSource)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    dependsOn(tasks["javadoc"])
+    classifier = "javadoc"
+    from(java.docsDir)
+}
+
+artifacts {
+    add("archives", sourcesJar)
+    add("archives", javadocJar)
+}
+
+if (hasProperty("signing.keyId")) {
+    signing {
+        setRequired({
+            !project.version.toString().endsWith("-SNAPSHOT")
+                    && gradle.taskGraph.hasTask("uploadArchives")
+        })
+        sign(configurations.archives)
+    }
+}
+
+val ossUser by project
+val ossPass by project
+
+tasks {
+    "afterReleaseBuild" {
+        dependsOn(tasks["uploadArchives"])
+    }
+    "uploadArchives"(Upload::class) {
+        repositories {
+            withConvention(MavenRepositoryHandlerConvention::class) {
+                mavenDeployer {
+                    beforeDeployment { let(signing::signPom) }
+                    withGroovyBuilder {
+                        "repository"("url" to uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")) {
+                            "authentication"("userName" to ossUser, "password" to ossPass)
+                        }
+                        "snapshotRepository"("url" to uri("https://oss.sonatype.org/content/repositories/snapshots/")) {
+                            "authentication"("userName" to ossUser, "password" to ossPass)
+                        }
+                    }
+
+                    pom.project {
+                        withGroovyBuilder {
+                            "packaging"("jar")
+                            "description"("Java implementation of the Loxone&trade; communication protocol (Web Socket)")
+                            "organization" {
+                                "name"("Smarteon Systems s.r.o")
+                                "url"("https://smarteon.cz")
+                            }
+                            "licenses" {
+                                "license" {
+                                    "name"("3-Clause BSD License")
+                                    "url"("http://opensource.org/licenses/BSD-3-Clause")
+                                    "distribution"("repo")
+                                }
+                            }
+                            "developers" {
+                                "developer" {
+                                    "name"("Jiří Mikulášek")
+                                    "email"("jiri.mikulasek@smarteon.cz")
+                                }
+                                "developer" {
+                                    "name"("Vojtěch Zavřel")
+                                    "email"("vojtech.zavrel@smarteon.cz")
+                                }
+                            }
+                            "scm" {
+                                "url"("git@github.com:Smarteon/loxone-java.git")
+                                "connection"("scm:git:git@github.com:Smarteon/loxone-java.git")
+                                "tag"("HEAD")
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }

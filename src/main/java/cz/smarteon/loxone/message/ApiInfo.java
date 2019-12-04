23513f7bcd6b52bfce462dcda54f037a03e0e549
xzavrel <info@vojtechzavrel.cz>
2019-12-04 15:59:51 +0100

    Fix ApiInfo problem in version 10.3.11.27

--- src/main/java/cz/smarteon/loxone/message/ApiInfo.java
+++ src/main/java/cz/smarteon/loxone/message/ApiInfo.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.util.regex.Matcher;
@@ -15,12 +16,22 @@ import java.util.regex.Pattern;
 @JsonDeserialize(using = ApiInfo.Deserializer.class)
 public class ApiInfo implements LoxoneValue {
 
+    /* MAC Address of the Miniserver */
     private final String mac;
+
+    /* Loxone Config Version */
     private final String version;
+    private Boolean eventSlots;
 
     public ApiInfo(final String mac, final String version) {
         this.mac = mac;
         this.version = version;
+    }
+
+    public ApiInfo(final String mac, final String version, final Boolean eventSlots) {
+        this.mac = mac;
+        this.version = version;
+        this.eventSlots = eventSlots;
     }
 
     public String getMac() {
@@ -31,6 +42,9 @@ public class ApiInfo implements LoxoneValue {
         return version;
     }
 
+    @Nullable
+    public Boolean getEventSlots() {return eventSlots;}
+
     @JsonValue
     private String jsonValue() {
         return "{'snr':'" + mac + "', 'version':'" + version + "'}";
@@ -39,13 +53,17 @@ public class ApiInfo implements LoxoneValue {
     public static class Deserializer extends JsonDeserializer<ApiInfo> {
 
         // the \\} is reported as redundant escape by Intellij, however android can't parse the regex without it :-(
-        private static final Pattern API_VALUE_PATTERN = Pattern.compile("\\{\\s*'snr'\\s*:\\s*'([^']+)'\\s*,\\s*'version'\\s*:\\s*'([^']+)'\\s*\\}");
+        // the regex is used because the content of ApiInfo is not real JSON as it uses ' char instead of ".
+        private static final Pattern API_VALUE_PATTERN = Pattern.compile("\\{\\s*'snr'\\s*:\\s*'([^']+)'\\s*,\\s*'version'\\s*:\\s*'([^']+)'\\s*,?\\s*(?:'hasEventSlots')?\\s*:?\\s*(.*)\\}");
 
         @Override
         public ApiInfo deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
             final String apiValue = p.readValueAs(String.class);
             final Matcher matcher = API_VALUE_PATTERN.matcher(apiValue);
             if (matcher.find()) {
+                if (matcher.groupCount() > 2) {
+                    return new ApiInfo(matcher.group(1), matcher.group(2), "true".equals(matcher.group(3)));
+                }
                 return new ApiInfo(matcher.group(1), matcher.group(2));
             } else {
                 throw JsonMappingException.from(p, "Can't parse api info using regex");

b7f6685d71cde467a4aa3dc4c9e5d29ffe9a642d
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2019-12-19 09:16:32 +0100

    Loxone events javadoc, tests and nullability

--- src/main/java/cz/smarteon/loxone/message/LoxoneEvent.java
+++ src/main/java/cz/smarteon/loxone/message/LoxoneEvent.java
@@ -1,15 +1,30 @@
 package cz.smarteon.loxone.message;
 
 import cz.smarteon.loxone.LoxoneUuid;
+import org.jetbrains.annotations.NotNull;
 
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Common predecessor of loxone events.
+ */
 public abstract class LoxoneEvent {
 
     protected final LoxoneUuid uuid;
 
-    protected LoxoneEvent(final LoxoneUuid uuid) {
-        this.uuid = uuid;
+    /**
+     * New instance
+     * @param uuid event uuid
+     */
+    protected LoxoneEvent(final @NotNull LoxoneUuid uuid) {
+        this.uuid = requireNonNull(uuid, "uuid can't be null");
     }
 
+    /**
+     * Get event uuid
+     * @return event uuid
+     */
+    @NotNull
     public LoxoneUuid getUuid() {
         return uuid;
     }
--- src/main/java/cz/smarteon/loxone/message/TextEvent.java
+++ src/main/java/cz/smarteon/loxone/message/TextEvent.java
@@ -1,21 +1,43 @@
 package cz.smarteon.loxone.message;
 
 import cz.smarteon.loxone.LoxoneUuid;
+import org.jetbrains.annotations.NotNull;
 
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Loxone event carrying text value.
+ */
 public class TextEvent extends LoxoneEvent {
     private final LoxoneUuid iconUuid;
     private final String text;
 
-    public TextEvent(final LoxoneUuid uuid, final LoxoneUuid iconUuid, final String text) {
+    /**
+     * Creates new instance.
+     * @param uuid event uuid
+     * @param iconUuid icon uuid
+     * @param text text value
+     */
+    public TextEvent(final @NotNull LoxoneUuid uuid, final @NotNull LoxoneUuid iconUuid, final @NotNull String text) {
         super(uuid);
-        this.iconUuid = iconUuid;
-        this.text = text;
+        this.iconUuid = requireNonNull(iconUuid, "iconUuid can't be null");
+        this.text = requireNonNull(text, "text can't be null");
     }
 
+    /**
+     * Icon uuid
+     * @return icon uuid
+     */
+    @NotNull
     public LoxoneUuid getIconUuid() {
         return iconUuid;
     }
 
+    /**
+     * Carried text value
+     * @return text value
+     */
+    @NotNull
     public String getText() {
         return text;
     }

8eafe2e0265fad43487fadb5de198911ed5a0660
Tom Cassimon <3518416+TCke83@users.noreply.github.com>
2022-07-28 19:39:19 +0200

    Introduced Room & Category deserialisation of loxone structure file + added methods in LoxoneApp to filter controls by Room, Category and both. Corrected equals for null safety and added extra requireNonNull on input uuid

--- src/main/java/cz/smarteon/loxone/app/LoxoneApp.java
+++ src/main/java/cz/smarteon/loxone/app/LoxoneApp.java
@@ -15,6 +15,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.requireNonNull;
 
@@ -26,14 +27,20 @@ public class LoxoneApp implements Serializable {
 
     private final Date lastModified;
     private final MiniserverInfo miniserverInfo;
+    private final Map<LoxoneUuid, Room> rooms;
+    private final Map<LoxoneUuid, Category> categories;
     private final Map<LoxoneUuid, Control> controls;
 
     @JsonCreator
     public LoxoneApp(@JsonProperty("lastModified") Date lastModified,
                      @JsonProperty("msInfo") MiniserverInfo miniserverInfo,
+                     @JsonProperty("rooms") Map<LoxoneUuid, Room> rooms,
+                     @JsonProperty("cats") Map<LoxoneUuid, Category> categories,
                      @JsonProperty("controls") Map<LoxoneUuid, Control> controls) {
         this.lastModified = requireNonNull(lastModified, "lastModified can't be null");
         this.miniserverInfo = requireNonNull(miniserverInfo, "miniserverInfo can't be null");
+        this.rooms = requireNonNull(rooms, "rooms can't be null");
+        this.categories = requireNonNull(categories, "categories can't be null");
         this.controls = requireNonNull(controls, "controls can't be null");
     }
 
@@ -50,6 +57,16 @@ public class LoxoneApp implements Serializable {
     @NotNull
     public MiniserverInfo getMiniserverInfo() {
         return miniserverInfo;
+    }
+
+    @NotNull
+    public Map<LoxoneUuid, Room> getRooms() {
+        return rooms;
+    }
+
+    @NotNull
+    public Map<LoxoneUuid, Category> getCategories() {
+        return categories;
     }
 
     @NotNull
@@ -119,5 +136,101 @@ public class LoxoneApp implements Serializable {
             }
         }
         return found;
+    }
+
+    /**
+     * Filters the controls with the room specified.
+     *
+     * @param room room to get the controls for
+     * @return collection of found controls assigned to given room or empty list if none found
+     */
+    @JsonIgnore
+    @NotNull
+    public List<Control> getControlsForRoom(final @NotNull Room room) {
+        requireNonNull(room, "room can't be null");
+        requireNonNull(room.getUuid(), "room uuid can't be null");
+        return getControls().values().stream()
+                .filter(c -> room.getUuid().equals(c.getRoomUuid()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Filters the controls with the category specified.
+     *
+     * @param category category to get the controls for
+     * @return collection of found controls assigned to given category or empty list if none found
+     */
+    @JsonIgnore
+    @NotNull
+    public List<Control> getControlsForCategory(final @NotNull Category category) {
+        requireNonNull(category, "category can't be null");
+        requireNonNull(category.getUuid(), "category uuid can't be null");
+        return getControls().values().stream()
+                .filter(c -> category.getUuid().equals(c.getCategoryUuid()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Filters the controls with the room and category specified.
+     *
+     * @param room room to get the controls for
+     * @param category category to get the controls for
+     * @return collection of found control assigned to given category and room or empty list if none found
+     */
+    @JsonIgnore
+    @NotNull
+    public List<Control> getControlsForRoomAndCategory(final @NotNull Room room, final @NotNull Category category) {
+        requireNonNull(room, "room can't be null");
+        requireNonNull(room.getUuid(), "room uuid can't be null");
+        requireNonNull(category, "category can't be null");
+        requireNonNull(category.getUuid(), "category uuid can't be null");
+        return getControls().values().stream()
+                .filter(c -> room.getUuid().equals(c.getRoomUuid()))
+                .filter(c -> category.getUuid().equals(c.getCategoryUuid()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Get the room object by its name. If zero or more than one are found it throws an exception.
+     *
+     * @param name the name of the room
+     * @return found room with given name
+     * @throws LoxoneException when zero or more than one room is found for the supplied name
+     */
+    @JsonIgnore
+    @NotNull
+    public Room getRoomByName(final @NotNull String name) {
+        List<Room> roomList = getRooms().values().stream()
+                .filter(r -> name.equalsIgnoreCase(r.getName()))
+                .collect(Collectors.toList());
+
+        if (roomList.size() == 1) {
+            return roomList.get(0);
+        } else if (roomList.isEmpty()) {
+            throw new LoxoneException("No room found with name " + name + "!");
+        } else {
+            throw new LoxoneException("Multiple rooms found <" + roomList.size() + "> for name " + name + "!");
+        }
+    }
+
+    /**
+     * Get the category object by its name. If zero or more than one are found it throws an exception.
+     *
+     * @param name the name of the category
+     * @return found category with given name
+     * @throws LoxoneException when zero or more than one category is found for supplied the name
+     */
+    public Category getCategoryByName(final @NotNull String name) {
+        List<Category> categoryList = getCategories().values().stream()
+                .filter(c -> name.equalsIgnoreCase(c.getName()))
+                .collect(Collectors.toList());
+
+        if (categoryList.size() == 1) {
+            return categoryList.get(0);
+        } else if (categoryList.isEmpty()) {
+            throw new LoxoneException("No category found with name " + name + "!");
+        } else {
+            throw new LoxoneException("Multiple categories found <" + categoryList.size() + "> for name " + name + "!");
+        }
     }
 }

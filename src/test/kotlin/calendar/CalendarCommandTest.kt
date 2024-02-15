3dfb6bc45c1b17a6d6be86fbbb37fa0e470c3e14
Tomáš Knotek <t.knotek22@gmail.com>
2024-02-15 16:18:00 +0100

    feat(calendar): delete entry command without creating CalEntry

--- src/test/kotlin/calendar/CalendarCommandTest.kt
+++ src/test/kotlin/calendar/CalendarCommandTest.kt
@@ -1,5 +1,6 @@
 package cz.smarteon.loxone.calendar
 
+import cz.smarteon.loxone.LoxoneUuid
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
@@ -7,7 +8,13 @@ import strikt.assertions.isEqualTo
 class CalendarCommandTest {
 
     @Test
-    fun `should create command`() {
+    fun `should create get command`() {
         expectThat(CalendarCommand.getEntries().command).isEqualTo("jdev/sps/calendargetentries/")
+    }
+
+    @Test
+    fun `should create delete command`() {
+        expectThat(CalendarCommand.deleteEntry(LoxoneUuid("00000000-0000-0000-0000000000000000")).command)
+            .isEqualTo("jdev/sps/calendardeleteentry/00000000-0000-0000-0000000000000000")
     }
 }
